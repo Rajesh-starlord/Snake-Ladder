2a13c276dccf9028c8655cd5bad9e15b8bb5e4af
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-06-09 20:09:31 +0530

    added uc-3

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,5 +2,16 @@
 #snake and ladder
 
 echo "Welcome to snake and ladder game:"
-rno=$(($(($RANDOM%6))+1));
-echo "you got $rno"
+echo "================================="
+dice_roll_val=$(($(($RANDOM%6))+1));
+echo "you got $dice_roll_val"
+option=$(($(($RANDOM%3))+1));
+if [[ $option -eq 1 ]];
+then
+	echo "no play"
+elif [[ $option -eq 2 ]];
+then
+	echo "got ladder"
+else
+	echo "got snake"
+fi
